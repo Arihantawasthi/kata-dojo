34d674e939b54192c5059db7e940fb71ee9e2892
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-05-30 00:35:51 +0530

    go: implemented binary search

--- go/main.go
+++ go/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 
 func indexOf(list []int, value int) int {
@@ -14,8 +16,34 @@ func indexOf(list []int, value int) int {
     return -1
 }
 
+
+func binarySearch(list []int, value int) int {
+    low := 0
+    high := len(list)
+    needle := (low + (low + high) / 2) 
+
+    for low < high {
+        if value > needle {
+            low = needle + 1
+        }
+        if (value < needle) {
+            high = needle
+        }
+        if (value == needle) {
+            return needle
+        }
+    }
+
+    return -1
+}
+
+
 func main() {
     numbers := []int{0, 2, 1, 10, 4, 19}
     output := fmt.Sprintf("Output of indexOf: %d\n", indexOf(numbers, -1))
+    fmt.Println(output)
+
+    numbers = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    output = fmt.Sprintf("Output of binarySearch: %d\n", indexOf(numbers, 1))
     fmt.Println(output)
 }
